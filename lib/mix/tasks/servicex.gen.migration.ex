6eb4bef6a070e27172f11f16f6db1aaeb665e512
Tuchiro Yoshimura <tcr.yoshimura@karabiner.tech>
2018-06-18 15:52:57 +0900

    migrationファイルのタイムスタンプずらす対応

--- lib/mix/tasks/servicex.gen.migration.ex
+++ lib/mix/tasks/servicex.gen.migration.ex
@@ -17,15 +17,19 @@ defmodule Mix.Tasks.Servicex.Gen.Migration do
     app_module = Mix.Project.config[:app] |> Atom.to_string() |> Macro.camelize()
     assigns = [app_module: app_module]
 
-    {:ok, current_time} = Timex.format(Timex.now(), "%Y%m%d%H%M%S", :strftime)
+    current_datetime = Timex.now()
+    time_1 = Timex.shift(current_datetime, seconds: +1)
+    {:ok, timestampe_1} = Timex.format(time_1, "%Y%m%d%H%M%S", :strftime)
+    time_2 = Timex.shift(current_datetime, seconds: +2)
+    {:ok, timestampe_2} = Timex.format(time_2, "%Y%m%d%H%M%S", :strftime)
 
   # create user migrations
   create_file(
-    Path.join([@migrations_file_path, "#{current_time}_servicex_craete_user.exs"]) |> Path.relative_to(Mix.Project.app_path),
+    Path.join([@migrations_file_path, "#{timestampe_1}_servicex_craete_user.exs"]) |> Path.relative_to(Mix.Project.app_path),
     user_template(assigns))
   # create grant migrations
   create_file(
-    Path.join([@migrations_file_path, "#{current_time}_servicex_craete_grant.exs"]) |> Path.relative_to(Mix.Project.app_path),
+    Path.join([@migrations_file_path, "#{timestampe_2}_servicex_craete_grant.exs"]) |> Path.relative_to(Mix.Project.app_path),
     user_template(assigns))
   end
 
