d8c785cb1ba0a5ba130404bbb49351c82be623a4
Tuchiro Yoshimura <tcr.yoshimura@karabiner.tech>
2018-06-18 18:51:14 +0900

    時刻系さん

--- lib/mix/tasks/servicex.gen.migration.ex
+++ lib/mix/tasks/servicex.gen.migration.ex
@@ -17,21 +17,35 @@ defmodule Mix.Tasks.Servicex.Gen.Migration do
     app_module = Mix.Project.config[:app] |> Atom.to_string() |> Macro.camelize()
     assigns = [app_module: app_module]
 
-    current_datetime = Timex.now()
-    time_1 = Timex.shift(current_datetime, seconds: +1)
-    {:ok, timestampe_1} = Timex.format(time_1, "%Y%m%d%H%M%S", :strftime)
-    time_2 = Timex.shift(current_datetime, seconds: +2)
-    {:ok, timestampe_2} = Timex.format(time_2, "%Y%m%d%H%M%S", :strftime)
+
+
+    #current_datetime = Timex.now()
+    #IO.puts("--- current_datetime -------")
+    #IO.inspect(current_datetime)
+    #IO.inspect(Timex.Timezone.local())
+    #time_1 = Timex.shift(current_datetime, seconds: +1)
+    #{:ok, timestampe_1} = Timex.format(time_1, "%Y%m%d%H%M%S", :strftime)
+    #time_2 = Timex.shift(current_datetime, seconds: +2)
+    #{:ok, timestampe_2} = Timex.format(time_2, "%Y%m%d%H%M%S", :strftime)
 
   # create user migrations
   create_file(
-    Path.join([@migrations_file_path, "#{timestampe_1}_servicex_craete_user.exs"]) |> Path.relative_to(Mix.Project.app_path),
+    Path.join([@migrations_file_path, "#{timestamp(1)}_servicex_craete_user.exs"]) |> Path.relative_to(Mix.Project.app_path),
     user_template(assigns))
   # create grant migrations
   create_file(
-    Path.join([@migrations_file_path, "#{timestampe_2}_servicex_craete_grant.exs"]) |> Path.relative_to(Mix.Project.app_path),
+    Path.join([@migrations_file_path, "#{timestamp(2)}_servicex_craete_grant.exs"]) |> Path.relative_to(Mix.Project.app_path),
     user_template(assigns))
   end
+
+  defp timestamp(add_sec) do
+    {{y, m, d}, {hh, mm, ss}} = :calendar.universal_time()
+    "#{y}#{pad(m)}#{pad(d)}#{pad(hh)}#{pad(mm)}#{pad(ss , add_sec )}"
+  end
+
+  defp pad(i,j \\ 0)
+  defp pad(i,j) when i < 10, do: <<?0, ?0 + ( i + j ) >>
+  defp pad(i,j), do: to_string(i + j)
 
   # template
   embed_template(:user, """
