9b47d8bf3487d17195e977ca538f26d026d5351d
Tuchiro Yoshimura <tcr.yoshimura@karabiner.tech>
2018-06-20 19:09:44 +0900

    バージョンアップ

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Servicex.Mixfile do
   def project do
     [
       app: :servicex,
-      version: "0.0.4",
+      version: "0.0.5",
       elixir: "~> 1.4",
       description: "This library is a summary of the functions that are generally required for Web service development.",
       elixirc_paths: elixirc_paths(Mix.env),
