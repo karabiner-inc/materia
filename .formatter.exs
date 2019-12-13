1e641cf3d60617e4ac62cb457b5ed589b8a990e3
mike.kodama <mike.kodama@karabiner.tech>
2019-12-13 15:12:58 +0900

    ふぉーまったー

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 # Used by "mix format"
 [
-  inputs: ["mix.exs", "{lib,test}/**/*.{ex,exs}", "priv/repo/migrations/**/*.{ex,exs}"],
+  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}", "priv/repo/migrations/**/*.{ex,exs}"],
   line_length: 120
 ]
